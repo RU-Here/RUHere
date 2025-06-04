9761f7e68faff2565322029f8223db1492d697c1
AadityaMunjal <aadityamunjal06@gmail.com>
2025-06-04 11:22:58 -0400

    pivot to native ios

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Aaditya Munjal on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/iosTests/iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  iosTests.swift
+//  iosTests
+//
+//  Created by Aaditya Munjal on 6/4/25.
+//
+
+import Testing
+@testable import ios
+
+struct iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
