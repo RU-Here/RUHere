435a34eb2f07f6f300276400b1d83ecbc981feab
JashP26 <jashp752@gmail.com>
2025-06-19 15:16:38 -0400

    Initial Location Services

---
+++ android/app/src/main/java/com/example/testapp/LocationService.kt
@@ -0,0 +1,27 @@
+package com.example.testapp
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+
+class LocationService : Service() {
+
+    private val ServiceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private lateinit var locationClient: LocationClient
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        locationClient = DefaultLocationClient(
+            applicationContext,
+            LocationServices.getFusedLocationProviderClient(applicationContext)
+        )
+    }
+}
