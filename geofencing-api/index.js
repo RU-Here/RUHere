b231c1df6185f8fce9f702d73fa50c6362be7a18
matthewjyhan <176155858+matthewjyhan@users.noreply.github.com>
2025-08-06 15:17:16 -0400

    Continue fixing getAuth

--- geofencing-api/index.js
+++ geofencing-api/index.js
@@ -4,22 +4,24 @@ const cors = require('cors');
 const path = require('path')
 const app = express();
 const geofenceRoutes = require('./routes/geofence');
-const getAuth = require('firebase-admin/auth')
+const admin = require('firebase-admin')
 
 const apiKeyMiddleware = (req, res, next) => {
   const idToken = req.headers.authorization;
-  getAuth()
+  if (!idToken) {
+    return res.status(401).send('Unauthorized: Missing token');
+  }
+
+  admin.auth()
   .verifyIdToken(idToken)
   .then((decodedToken) => {
     const uid = decodedToken.uid;
-    // ...
+    next();
   })
   .catch((error) => {
     // Handle error
     res.status(401).send('Unauthorized')
   });
-  
-  next();
 };
 
 app.use(express.urlencoded({ extended: true })); 
